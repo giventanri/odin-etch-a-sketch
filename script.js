64e3241ec65d58d00963d755fa7ec7a3291db9b7
giventanri <given.tanri@my.wheaton.edu>
2022-10-17 16:37:18 -0500

    Implement hover effect on grid

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let container = document.querySelector('.grid-container')
 
 for (let i = 0; i < 256; i++) {
     let grid = document.createElement('div');
+
+    // costumize grid here
+        
     grid.setAttribute('class', 'grid-item');
     grid.setAttribute('id', 'grid-item-' + i);
 
-    // costumize grid here
+    grid.addEventListener('mouseover', () => {
+        grid.classList.add('grid-item-hovered');
+    });
 
     container.appendChild(grid);
 }
